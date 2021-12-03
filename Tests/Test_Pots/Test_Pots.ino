ed5e8aad61c4e408c1b15ea2612d5fab43545057
gannaramu <ganna.ramu@gmail.com>
2021-12-02 19:44:19 -0700

    update Pots Test

--- Tests/Test_Pots/Test_Pots.ino
+++ Tests/Test_Pots/Test_Pots.ino
@@ -1,8 +1,8 @@
 /*
-this program taken from arduino Example .
+  this program taken from arduino Example .
   modified by By Mohannad Rawashdeh
   http://www.genotronex.com
-https://www.instructables.com/
+  https://www.instructables.com/
 
   This code used to control the digital potentiometer
   MCP41100 connected to  arduino Board
@@ -16,45 +16,77 @@ https://www.instructables.com/
 #include <SPI.h>
 byte address = 0x00;
 //int CS= 7;
-int i=0;
+int i = 0;
 
 void setup()
 {
-  pinMode (8, OUTPUT);
+  pinMode(7, OUTPUT);
+  pinMode(8, OUTPUT);
+  pinMode(9, OUTPUT);
+  pinMode(10, OUTPUT);
+  digitalWrite(7, HIGH);
+  digitalWrite(8, HIGH);
+  digitalWrite(9, HIGH);
+  digitalWrite(10, HIGH);
+
   SPI.begin();
-  // adjust high and low resistance of potentiometer
-  // adjust Highest Resistance .
-   digitalPotWrite(0x00,8);
-   delay(1000);
-
-      // adjust  wiper in the  Mid point  .
-   digitalPotWrite(0x80,8);
-   delay(1000);
-
-   // adjust Lowest Resistance .
-   digitalPotWrite(0xFF,8);
-   delay(1000);
+  //  // adjust high and low resistance of potentiometer
+  //  // adjust Highest Resistance .
+  //   digitalPotWrite(0x00,7);
+  //   delay(1000);
+  //
+  //      // adjust  wiper in the  Mid point  .
+  //   digitalPotWrite(0x80,7);
+  //   delay(1000);
+  //
+  //   // adjust Lowest Resistance .
+  //   digitalPotWrite(0xFF,7);
+  //   delay(1000);
 }
 
 void loop()
 {
-    for (i = 0; i <= 255; i++)
-    {
-      digitalPotWrite(i,7);
-      delay(10);
+  for (i = 0; i <= 255; i++)
+  {
+    digitalPotWrite(i, 7);
+    delay(10);
+  }
+  delay(50);
+  MCP41HV_SetTerminals(3, 7);
+  delay(1000);
+  MCP41HV_SetTerminals(7, 7);
+  for (i = 255; i >= 0; i--)
+  {
+    if (i == 128) {
+      MCP41HV_SetTerminals(6, 7);
+      delay(1000);
+      MCP41HV_SetTerminals(7, 7);
     }
-    delay(50);
-    for (i = 255; i >= 0; i--)
-    {
-      digitalPotWrite(i,7);
-      delay(10);
-    }
+    digitalPotWrite(i, 7);
+
+
+    delay(10);
+  }
+
+
 }
 
-int digitalPotWrite(int value,int CS)
+int digitalPotWrite(int value, int CS)
 {
   digitalWrite(CS, LOW);
   SPI.transfer(address);
   SPI.transfer(value);
   digitalWrite(CS, HIGH);
+  return 1;
+}
+
+void MCP41HV_SetTerminals(int TCON_Value, int CS)
+{
+  digitalWrite(CS, LOW);
+  SPI.transfer(0x40); //Write to TCON Register
+  SPI.transfer(TCON_Value + 8);
+  SPI.transfer(0x4C); //Read Command
+  uint8_t result = SPI.transfer(0xFF); //Rea
+  digitalWrite(CS, HIGH);
+
 }
